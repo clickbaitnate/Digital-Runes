3286333ed9bd5f2ceb6708dba420188087e4017e
ogclickbaitnate <nathan.marotz@gmail.com>
2019-12-16 00:13:39 -0600

    Work In Progress

---
+++ script.js
@@ -0,0 +1,19 @@
+var rune;
+
+function getRandomInt(max) {
+return Math.floor(Math.random() * Math.floor(max));
+}
+
+ switch (getRandomInt(3)) {
+  case 0:   
+    console.log("Fehu");
+    break;
+  case 1: 
+    console.log("Uruz");
+    break;
+  case 2:
+    console.log("Thurisaz"); 
+
+ };
+ 
+ ;
